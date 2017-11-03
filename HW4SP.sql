785b1d87d48e34477f7a18fcb2952cf267d58506
malam1 <31459615+malam1@users.noreply.github.com>
2017-11-02 18:10:40 -0600

    Add files via upload

---
+++ HW4SP.sql
@@ -0,0 +1,44 @@
+create or replace function Flight_Sum()
+	return varchar(50) as $AirlineName$, 
+			int as $totalFlights$, 
+			float as $avgDistance$,
+			float as $passengersToDistance$,
+			float as $freightToDistance$,
+			int as $corrCoeffPassengersToDistance$,
+			int as $corrCoeffFreightToDistance$
+	
+	declare AirlineName	varchar(50);
+			totalFlights int;
+			avgDistance float;
+			passengersToDistance float;
+			freightToDistance float;
+			corrCoefPtoD int;
+			corrCoefFtoD int;
+
+BEGIN
+	select distinct UNIQUE_CARRIER_NAME from Carrier into AirlineName;
+	select count(*) from Flight_Sum where Flight_Sum.UNIQUE_CARRIER_NAME = AirlineName into totalFlights;
+	select avg(Distance) from Flight_Sum where Flight_Sum.UNIQUE_CARRIER_NAME = AirlineName into avgDistance;
+	select passengers / Distance from Flight_Sum where Flight_Sum.UNIQUE_CARRIER_NAME = AirlineName into passengersToDistance;
+	select freight / Distance from Flight_Sum where Flight_Sum.UNIQUE_CARRIER_NAME = AirlineName into freightToDistance; 
+	
+	if passengersToDistance > 1
+	Then corrCoefPToD = 1;
+	Else corrCoefPToD = -1;
+	End if;
+
+	if freightToDistance > 1
+	Then corrCoefFToD = 1;
+	Else corrCoefFToD = -1;
+	End if;
+
+End
+
+
+
+
+
+
+
+			
+
